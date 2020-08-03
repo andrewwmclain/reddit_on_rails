0dc3096a7c09b335e0325d47875fc3db358f6923
andrewwmclain <mclaina1@spu.edu>
2020-08-03 16:40:33 -0700

    link submission

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,10 +1,36 @@
 class LinksController < ApplicationController
+    def index
+
+    end
+
     def show
     end
 
     def new
+        @link = Link.new
     end
     
     def create
+        @link = Link.new(link_params)
+        @link.save
+
+        flash.notice = "Link '#{@link.title}' Created!"
+
+        redirect_to links_path#(@link)
+        
+        # @link = Link.new(link_params)
+        # respond_to do |format|
+        #       if @link.save
+        #         format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        #         format.json { render :show, status: :created, location: @link }
+        #       else
+        #         format.html { render :new }
+        #         format.json { render json: @link.errors, status: :unprocessable_entity }
+        #       end
+        # end
+    end
+
+    def link_params
+        params.require(:link).permit(:title, :url)
     end
 end
