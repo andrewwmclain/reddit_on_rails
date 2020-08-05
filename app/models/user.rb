49c87febd6b4db225bbcb595d457776d29944dd2
andrewwmclain <mclaina1@spu.edu>
2020-08-05 14:48:50 -0700

    get rid of attr_accessor override

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :email, :username
+  # attr_accessor :email, :username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
