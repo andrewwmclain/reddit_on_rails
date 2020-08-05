b76e3904cddfa9884f78c3225aeb278ca6d93dd8
andrewwmclain <mclaina1@spu.edu>
2020-08-05 15:21:46 -0700

    database config and remove attr_accessor from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  # attr_accessor :email, :username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
