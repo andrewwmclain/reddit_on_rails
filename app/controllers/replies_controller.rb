00fa7f41057bdf679202e548719902dec37dd936
andrewwmclain <mclaina1@spu.edu>
2020-08-14 14:04:04 -0700

    add replies to comments

---
+++ app/controllers/replies_controller.rb
@@ -0,0 +1,23 @@
+class RepliesController < ApplicationController
+    def new
+        @reply = Comment.new 
+        @comment = Comment.find(params[:comment])
+        @link = Link.find(params[:link])
+    end
+
+    def create
+        @user = current_user
+        @reply = Comment.new(reply_params)
+        @reply.user_id = @user.id
+        if @reply.save
+            flash.notice = "Reply Created!"
+            redirect_to link_path(@reply.link_id) and return
+        else 
+            render :new and return
+        end
+    end
+
+    def reply_params
+        params.require(:comment).permit(:message, :user_id, :link_id, :parent_id)
+    end
+end
