4736469bb4f71d564fccea6fe9b4e5bb3b3d916e
andrewwmclain <mclaina1@spu.edu>
2020-08-18 09:36:19 -0700

    quotation marks

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,5 @@
 class VotesController < ApplicationController
     def create
-        # @vote = Vote.new(vote_params)
-
-        # if @vote.save
-        #     flash.notice = "Voted!"
-        # else 
-        #     flash.notice = "Cannot vote"
-        # end
-        # redirect_to pages_path
-
         @vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
         if @vote
             @vote.up = params[:vote][:up]
