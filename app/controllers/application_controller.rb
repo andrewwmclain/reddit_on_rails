0f020f03043c6489a7fc7a3051b8119790dc52c0
andrewwmclain <mclaina1@spu.edu>
2020-08-21 10:32:44 -0700

    carrierwave can update avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   
       def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :terms, :location])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :terms, :location])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :terms, :location, :avatar])
       end
 end
