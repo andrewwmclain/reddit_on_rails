78fc2699163e1f5c9ab3262273faeac25909e8b5
andrewwmclain <mclaina1@spu.edu>
2020-08-28 16:42:30 -0700

    delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,11 @@ class CommentsController < ApplicationController
         end
     end
 
+    def destroy
+        Comment.find(params[:id]).destroy
+        redirect_back(fallback_location: links_path) and return
+    end
+
     def comment_params
         params.require(:comment).permit(:message, :user_id, :link_id)
     end
